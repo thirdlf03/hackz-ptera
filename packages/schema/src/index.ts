5541f27fccae758abe2afa83c12eb6871d23114a
KoueiAmano <79039406+KoueiAmano@users.noreply.github.com>
2025-12-20 17:55:42 +0900

    Feature/add ai personality schema (#36)
    * Personality, AIResponce のスキーマを定義
    
    * chore: フォーマッター適用
    
    ---------
    
    Co-authored-by: take <111164109+Take-613@users.noreply.github.com>

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,4 +2,6 @@ export * from "./user";
 export * from "./common";
 export * from "./chess";
 export * from "./voice-input";
+export * from "./aiResponse";
+export * from "./personality";
 export * from "./backend-chess";
---
+++ packages/schema/src/personality.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const personalitySchema = z.object({
+  obedience: z
+    .number()
+    .min(0)
+    .max(1)
+    .refine((n) => Math.abs(n * 10 - Math.round(n * 10)) < Number.EPSILON),
+  aggressiveness: z
+    .number()
+    .min(0)
+    .max(1)
+    .refine((n) => Math.abs(n * 10 - Math.round(n * 10)) < Number.EPSILON),
+  fear: z
+    .number()
+    .min(0)
+    .max(1)
+    .refine((n) => Math.abs(n * 10 - Math.round(n * 10)) < Number.EPSILON),
+  randomness: z
+    .number()
+    .min(0)
+    .max(1)
+    .refine((n) => Math.abs(n * 10 - Math.round(n * 10)) < Number.EPSILON),
+});
+
+export type Personality = z.infer<typeof personalitySchema>;
