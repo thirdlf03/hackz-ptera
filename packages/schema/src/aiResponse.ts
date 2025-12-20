6c4b9a907593c75f1754be93a9cc64a794d35939
KoueiAmano <fil.neo118@gmail.com>
2025-12-20 17:24:42 +0900

    chore: フォーマッター適用

--- packages/schema/src/aiResponse.ts
+++ packages/schema/src/aiResponse.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const AIResponseSchema = z.object({
-  "from": z.string().regex(/^[a-h][1-8]$/),
-  "to": z.string().regex(/^[a-h][1-8]$/),
+  from: z.string().regex(/^[a-h][1-8]$/),
+  to: z.string().regex(/^[a-h][1-8]$/),
   attack: z.boolean(),
-  reason: z.string()
+  reason: z.string(),
 });
 
 export type AIResponse = z.infer<typeof AIResponseSchema>;
--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,5 +2,5 @@ export * from "./user";
 export * from "./common";
 export * from "./chess";
 export * from "./voice-input";
-export * from "./aiResponse"
-export * from "./personality"
+export * from "./aiResponse";
+export * from "./personality";
