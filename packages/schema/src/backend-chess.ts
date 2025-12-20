3aeba227e708f04a3799d2032080af34f453a9f4
TK-613 <take040613@gmail.com>
2025-12-20 18:06:16 +0900

    fix(schema):unify personality schema

--- packages/schema/src/backend-chess.ts
+++ packages/schema/src/backend-chess.ts
@@ -1,19 +1,13 @@
 import { z } from "zod";
+import { personalitySchema } from "./personality";
 
 // Schema for chess Personalities
 
-export const ChessPersonalitySchema = z.object({
-  obedience: z.number().min(0).max(1).describe("駒の指示に従う度合いを示す数値(0.0~1.0)"),
-  aggressiveness: z.number().min(0).max(1).describe("攻撃的な動きをする度合いを示す数値(0.0~1.0)"),
-  fear: z.number().min(0).max(1).describe("恐怖心の度合いを示す数値(0.0~1.0)"),
-  randomness: z.number().min(0).max(1).describe("ランダムな動きをする度合いを示す数値(0.0~1.0)"),
-});
-
-export type ChessPersonality = z.infer<typeof ChessPersonalitySchema>;
+export type ChessPersonality = z.infer<typeof personalitySchema>;
 
 export const PieceStatesSchema = z.object({
   piece: z.enum(["pawn", "rook", "knight", "bishop", "queen", "king"]),
-  personality: ChessPersonalitySchema,
+  personality: personalitySchema,
   position: z.string().regex(/[a-h][1-8]/),
   team: z.union([z.literal(0), z.literal(1)]),
 });
