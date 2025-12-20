574ec809dbe64925efa5bea9eab27649bb1f8212
thirdlf03 <thirdlf03@gmail.com>
2025-12-21 00:50:33 +0900

    fix: check-types

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,7 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import {
-  CreateUserSchema,
-  type User,
   UsersResponseSchema,
-  UserCreatedResponseSchema,
   VoiceInputRequestSchema,
   VoiceInputSuccessResponseSchema,
   VoiceInputErrorResponseSchema,
