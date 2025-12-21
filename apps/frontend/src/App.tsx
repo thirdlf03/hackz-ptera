6d6ad2c9eb461d84b6c7146fdaf9b84adff4f049
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-12-21 15:05:34 +0900

    fix(ui):text direction

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -8,7 +8,7 @@ import "@/App.css";
 function App() {
   return (
     <>
-      <div className="app-container">
+      <div className='app-container'>
         <Canvas>
           {/* ライトをCanvas直下に配置 */}
           <directionalLight position={[0, 0, 15]} />
@@ -16,20 +16,22 @@ function App() {
 
           <Center position={[0, 3.1, 0.3]}>
             <Text3D
-              font="https://threejs.org/examples/fonts/helvetiker_regular.typeface.json"
+              font='https://threejs.org/examples/fonts/helvetiker_regular.typeface.json'
               size={0.5}
               height={0.1}
               bevelEnabled={true} // ベベルを有効化
               bevelThickness={0.1}
               rotation={[Math.PI / 7, 0, 0]}
             >
-              AI Chess
+              自我チェス
               {/* マテリアルを追加 */}
-              <meshStandardMaterial color={new THREE.Color("rgba(249, 209, 8, 1)")} />
+              <meshStandardMaterial
+                color={new THREE.Color("rgba(249, 209, 8, 1)")}
+              />
             </Text3D>
           </Center>
 
-          <Board className="container-3d" />
+          <Board className='container-3d' />
         </Canvas>
       </div>
     </>
---
+++ apps/frontend/src/features/chess/compomemt/LinkVoiceAndID.tsx
@@ -0,0 +1,1 @@
+import type { Piece, VoiceInput } from "@repo/schema";
